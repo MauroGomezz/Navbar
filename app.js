b33599beaec1492ee3021a8ca16cb6ae8651f78b
Kuma <98374456+MauroGomezz@users.noreply.github.com>
2022-06-16 22:42:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,3 @@ const list = document.querySelector(".list")
 icon.addEventListener('click', () => {
     list.classList.toggle("links")
 })
-
-window.addEventListener('click', (e) =>{
-	if(list.classList.contains('links') && e.target != list && e.target != icon) {
-		list.classList.toggle("links")
-	}
-})
